aa22cc037063b8b5a191dde759f1b3b0f477720e
David Carpenter <caprenter@gmail.com>
2012-10-09 11:33:52 +0100

    Adds missing semicolons to example.settings.php

--- example.settings.php
+++ example.settings.php
@@ -4,6 +4,6 @@ $host = "http://localhost/Webs/validate-me/";
 date_default_timezone_set('GMT');
 //in the logs/ directory there is a sample log viewer that can be run from the command line.
 //you may want to store your log files in that directory, but feel free to put them elsewhere
-$log_file = "path/to/log/file" //e.g. /var/www/yourapplication/logs/log.txt
-$google_analytics_code = "" //paste your google analytics code here - remember to escape double quotes e.g. <script type=\"text/javascript\">
+$log_file = "path/to/log/file"; //e.g. /var/www/yourapplication/logs/log.txt
+$google_analytics_code = ""; //paste your google analytics code here - remember to escape double quotes e.g. <script type=\"text/javascript\">
 ?>
