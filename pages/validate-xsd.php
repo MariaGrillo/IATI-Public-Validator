0ef891fc3af0051c5fe92d29d3db2df036212fa0
David Carpenter <caprenter@gmail.com>
2014-08-18 18:03:34 +0100

    Adds better messages to users about version detection
    Needs tests writing, but test xml has been added to the tests
    directory that we can use.

--- pages/validate-xsd.php
+++ pages/validate-xsd.php
@@ -8,17 +8,8 @@
 */
 
 require_once 'pages/validate-xsd_functions.php';
-require_once 'functions/check_iati_version.php';
+require_once 'functions/detect_iati_version.php';
 ?>
-<?php if(isset($error_msg)) :?>
-		<div class="alert alert-error">
-			<button type="button" class="close" data-dismiss="alert">×</button>
-			<strong>Sorry.</strong> We can't test that file.<br/>
-			<?php echo $error_msg; ?>
-		</div>
-<?php endif; ?>	
-	
-
 <?php if( !isset($_SESSION['uploadedfilepath']) ) :?>
 	 <?php header('Location: index.php'); ?>
 <?php else: ?>
@@ -28,29 +19,45 @@ require_once 'functions/check_iati_version.php';
       $xml = get_xml($file_path);
       if($xml === FALSE) return FALSE; // Need this to prevent entity security problem
 
-      
+      $detected_version = detect_iati_version($xml);
+
       //Get the right schema to validate against
       //Has the version been set by the user to a valid IATI version?
       //echo $_SESSION["version"];
       if(!isset($version) || $version == "auto") { //We put this in place so tests work! tests.php specifies the version already by this point.
-      if (isset($_SESSION["version"]) && in_array($_SESSION["version"],$iati_versions)) {
-        //$detected_version = check_iati_version($xml);
-        $version = $_SESSION["version"];
-        //echo $version;
-      } else {
-        //..else..lets try to detect the version
-        //NB id user has selected auto detect, then we end up here as well
-        $version = check_iati_version($xml);
-        //echo $version;
-      } 
-      //If by now we don't have a valid version...
-      if(!isset($version) || $version==FALSE) {
-        //Use the default. Default is current live version of the standard.
-        //echo "true"; echo $current_version;
-        $version = $current_version; //$current_version is declared at the top of index.php
-      }
+        if (isset($_SESSION["version"]) && in_array($_SESSION["version"],$iati_versions)) {
+          //$detected_version = check_iati_version($xml);
+          $version = $_SESSION["version"];
+          //echo $version;
+        } else {
+          //..else..lets try to detect the version
+          //NB if user has selected auto detect, then we end up here as well
+          //Check the detected version is a valid version
+          if (!in_array($detected_version,$iati_versions)) {
+            $version = $current_version;
+            if (isset($detected_version) && $detected_version !=NULL) {
+              if (isset($error_msg)) {
+                 $error_msg .= '<br/>';
+              } else {
+                  $error_msg = '';
+              }
+              $error_msg .= htmlspecialchars($detected_version) . " is not a valid version of the IATI Standard";
+            }
+          } else {
+            $version = $detected_version;
+          }
+          //echo $version;
+        } 
+        //If by now we don't have a valid version...
+        if(!isset($version) || $version==FALSE) {
+          //Use the default. Default is current live version of the standard.
+          //echo "true"; echo $current_version;
+          $version = $current_version; //$current_version is declared at the top of index.php
+        }
       }
       //echo $version; //die;
+      
+      
         
 			if ($xml->getElementsByTagName("iati-organisation")->length == 0) {
 			$xsd = "iati-schemas/" . $version . "/iati-activities-schema.xsd";
@@ -63,6 +70,15 @@ require_once 'functions/check_iati_version.php';
       $valid = $xml->schemaValidate($xsd); 
 			
 		?>
+    
+    <?php if(isset($error_msg)) :?>
+		<div class="alert alert-error">
+			<button type="button" class="close" data-dismiss="alert">×</button>
+			<strong>Sorry.</strong> There are problems with this file.<br/>
+			<?php echo $error_msg; ?>
+		</div>
+    <?php endif; ?>	
+    
 		<h2>Validation against the IATI <?php echo $schema; ?> Schema (version <?php echo $version; ?>)</h2>
 		<ul class="nav nav-tabs" id="myTab">
 		  <li class="active"><a href="#status">Status</a></li>
@@ -80,13 +96,59 @@ require_once 'functions/check_iati_version.php';
 						<div class="span5">-->
 							<?php if ($valid == TRUE): ?>
 								<h3 class="success">Success</h3>
-								<div class="alert alert-success">This file validates against the IATI <?php echo $schema; ?> Schema (version <?php echo $version; ?>) </div>
+								<div class="alert alert-success">This file validates against the IATI <?php echo $schema; ?> Schema version <?php echo $version; ?></div>
+                
+                <?php 
+                  if (isset($detected_version) && $detected_version !=NULL) {
+                    if (!in_array($detected_version,$iati_versions)) {
+                      echo '<h3 class="fail">Fail</h3>';
+                      echo '<div class="alert alert-error">';
+                      echo 'We detected version '. htmlspecialchars($detected_version) . ' in your file.<br/>You should use a recognised version of the schema.';
+                      if ($valid == TRUE && $version != $detected_version) {
+                         echo '<br/>You could consider declaring ' . $version . ' instead.';
+                       }
+                      echo '</div>';
+                    } else {
+                       echo '<h3 class="success">Success</h3>';
+                       echo '<div class="alert alert-success">';
+                       echo 'We detected version '. htmlspecialchars($detected_version) . ' declared in your file which is a recognised version of the schema.';
+                       if ($valid == TRUE && $version != $detected_version && floatval($version) > floatval($detected_version)) {
+                         echo '<br/>You could consider declaring ' . $version . ' instead.';
+                       }
+                       echo '</div>';
+                    }
+                  }
+                ?>
+
 							<?php else: ?>
 								<h3 class="fail">Fail</h3>
 								<div id="intext">
 									<div class="alert alert-error">This file does NOT validate against the IATI <?php echo $schema; ?> Schema (version <?php echo $version; ?>)</div>
 									<?php echo libxml_error_count(); ?> <br/><br/>
 									See <a href="#extra">Extra info</a> for details about the errors.
+                  
+                  <?php 
+                  if (isset($detected_version) && $detected_version !=NULL) {
+                    if (!in_array($detected_version,$iati_versions)) {
+                      echo '<h3 class="fail">Fail</h3>';
+                      echo '<div class="alert alert-error">';
+                      echo 'We detected version '. htmlspecialchars($detected_version) . ' in your file.<br/>You should use a recognised version of the schema.';
+                      if ($valid == TRUE && $version != $detected_version) {
+                         echo '<br/>You could consider declaring ' . $version . ' instead.';
+                       }
+                      echo '</div>';
+                    } else {
+                       echo '<h3 class="success">Success</h3>';
+                       echo '<div class="alert alert-success">';
+                       echo 'We detected version '. htmlspecialchars($detected_version) . ' declared in your file which is a recognised version of the schema.';
+                       if ($valid == TRUE && $version != $detected_version && floatval($version) > floatval($detected_version)) {
+                         echo '<br/>You could consider declaring ' . $version . ' instead.';
+                       }
+                       echo '</div>';
+                    }
+                  }
+                ?>
+                
 								</div>
 									See <a href="<?php echo $host; ?>common_errors.php">Common errors</a> for help in understanding the errors.
 								
