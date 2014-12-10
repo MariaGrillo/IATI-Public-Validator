MariaGrillo/IATI-Public-Validator

commit a1884ca7a5e62ffb630829acd012228941e1f0a0

Some w3c validation compliance fixes
Fixes label problems
Removes some badly formatted comments
Fixes stray legend tag
Closes fieldset tag
Fixes label for= error

index.php
@@ -75,17 +75,19 @@ include "header.php";
         <!--Version Switcher-->
 				<div class="well sidebar-nav">
           <form method="get" action="index.php">
-            <legend>Version</legend>
-            <label for="version">Schema version<br />
-            <select id="version-drop-down" name="version" class="span1">
-              <option <?php if (!isset($_SESSION["version"]) || $_SESSION["version"] == "auto") { echo 'selected="selected"'; } ?> value="auto">Auto Detect</option>
-            <?php foreach ($iati_versions as $version) { ?>
-              <option <?php if (isset($_SESSION["version"]) && $_SESSION["version"] == $version) { echo 'selected="selected"'; } ?> value="<?php echo $version ?>"><?php echo $version ?></option>
-            <?php } ?>
-            </select>
-            <button id="switch" type="submit" class="btn btn-primary">Switch</button>
-            <?php if (isset($test) && $test != "default" && in_array($test,$tests)) { echo '<input type="hidden" name="test" value="' . $test . '" />'; } ?>
-            <!--<input type="submit" value="Submit" />-->
+            <fieldset>
+              <legend>Version</legend>
+              <label for="version-drop-down">Schema version</label>
+              <select id="version-drop-down" name="version-drop-down" class="span1">
+                <option <?php if (!isset($_SESSION["version"]) || $_SESSION["version"] == "auto") { echo 'selected="selected"'; } ?> value="auto">Auto Detect</option>
+              <?php foreach ($iati_versions as $version) { ?>
+                <option <?php if (isset($_SESSION["version"]) && $_SESSION["version"] == $version) { echo 'selected="selected"'; } ?> value="<?php echo $version ?>"><?php echo $version ?></option>
+              <?php } ?>
+              </select>
+              <button id="switch" type="submit" class="btn btn-primary">Switch</button>
+              <?php if (isset($test) && $test != "default" && in_array($test,$tests)) { echo '<input type="hidden" name="test" value="' . $test . '" />'; } ?>
+              <!--<input type="submit" value="Submit" />-->
+            </fieldset>
           </form>
         </div> 
         

header.php
@@ -61,11 +61,11 @@
       <div class="row">
         <div class="span10">
           <p class="lead">
-            <a href="<?php echo $host; ?>"><img src="assets/img/logo.png" width="" height="" alt="IATI Logo" /></a>
+            <a href="<?php echo $host; ?>"><img src="assets/img/logo.png" width="269" height="70" alt="IATI Logo" /></a>
             IATI Public Validator
           </p>
         </div>
-        <div span="2" class="reset">
+        <div class="span2 reset">
           <?php if (isset($_SESSION['uploadedfilepath'])) :?>
             <p class="lead"><br/><a href="<?php echo $host; ?>?test=reset" class="btn btn-large btn-success">Load New File</a></p>
           <?php endif; ?>

pages/front.php
@@ -39,59 +39,46 @@ if( (sizeof($_FILES)==0 && !isset($_SESSION['uploadedfilepath']) || isset($error
 				<div class="tab-pane active" id="status">
 					<div class="span6">
 						<form action="index.php" method="post" enctype="multipart/form-data"  >
-							<legend>Upload File</legend>
-							<label for="file">File:</label>
-							<input type="file" name="file" class="span5"/>
-							<span class="help-block">Upload an XML file of IATI data.</span>
-							<!--<input type="submit" name="submit" value="Submit" />-->
-							<button type="submit" class="btn btn-primary">Upload</button>
+              <fieldset>
+                <legend>Upload File</legend>
+                <label for="file">File:</label>
+                <input type="file" name="file" id="file" class="span5"/>
+                <span class="help-block">Upload an XML file of IATI data.</span>
+                <button type="submit" class="btn btn-primary">Upload</button>
+              </fieldset>
 						</form>
 					</div>
 				</div>
-				<div class="tab-pane" id="file">
+				<div class="tab-pane" id="file-tab">
 					<!--<div class="span1">OR:</div>-->
 					<div class="span6">
 						<form method="post" action="index.php">
-							<legend>Fetch data from the Web</legend>
-							<label for="url">URL of file:<br />
-							<input type="text" placeholder="Paste URL here" name="url" class="span5" /> 
-							<span class="help-block">Enter an address of an IATI compliant XML file.</span>
-							<button type="submit" class="btn btn-primary">Fetch Data</button>
-							<!--<input type="submit" value="Submit" />-->
+              <fieldset>
+                <legend>Fetch data from the Web</legend>
+                <label for="url">URL of file:</label>
+                <input type="text" placeholder="Paste URL here" name="url" id="url" class="span5" /> 
+                <span class="help-block">Enter an address of an IATI compliant XML file.</span>
+                <button type="submit" class="btn btn-primary">Fetch Data</button>
+              </fieldset>
 						</form>
 					</div>
 				</div>
 				<div class="tab-pane" id="extra">
 					<div class="span6">
 						<form action="index.php" method="post">
-							<legend>Paste XML</legend>
-							<label for="paste">XML:</label>
-							<textarea rows="8" class="span5" name="paste" id="paste"></textarea>
-							<span class="help-block">Paste your XML here.</span>
-							<!--<input type="submit" name="submit" value="Submit" />-->
-							<button type="submit" class="btn btn-primary">Submit</button>
+              <fieldset>
+                <legend>Paste XML</legend>
+                <label for="paste">XML:</label>
+                <textarea rows="8" class="span5" name="paste" id="paste"></textarea>
+                <span class="help-block">Paste your XML here.</span>
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </fieldset>
 						</form>
 					</div>	
 				</div>	
 			</div>	
 		</div>
 	</div>
-	<!--<hr>
-	<div>OR:</div>
-	<div class="row">
-		<div class="span10" style="margin-left:0">
-			<div class="span8">
-				<form action="index.php" method="post">
-					<legend>Paste XML</legend>
-					<label for="file">XML:</label>
-					<textarea rows="8" class="span5" name="paste" id="paste"></textarea>
-					<span class="help-block">Paste your XML here.</span>
-					<!--<input type="submit" name="submit" value="Submit" />-->
-					<!--<button type="submit" class="btn btn-primary">Submit</button>
-				</form>
-			</div>			
-		</div>
-	</div>-->
   <!--Notification Area-->
   <hr />
   <div class="row">

commit 38a2f2c5d6df866f9cc4b62ea03fc083599b97f1

[#85] Fixes bug on version switcher
Introduced in https://github.com/IATI/IATI-Public-Validator/commit/a1884ca7a5e62ffb630829acd012228941e1f0a0

index.php
@@ -7,13 +7,13 @@ include "vars.php"; // contains definitions such as the test pages, and iati sta
 include "functions/index.php"; // contains the extra functions needed for this index page
 
 //Sanitize the $_GET vars
-if (isset($_GET['version'])) {
-  if (filter_var($_GET['version'], FILTER_VALIDATE_FLOAT)) {
-    if ( in_array($_GET['version'], $iati_versions) ) {
-      $_SESSION["version"] = $_GET['version'];
+if (isset($_GET['version-drop-down'])) {
+  if (filter_var($_GET['version-drop-down'], FILTER_VALIDATE_FLOAT)) {
+    if ( in_array($_GET['version-drop-down'], $iati_versions) ) {
+      $_SESSION["version"] = $_GET['version-drop-down'];
     }
   } else {
-    if ($_GET['version'] === "auto") {
+    if ($_GET['version-drop-down'] === "auto") {
       $_SESSION["version"] = "auto";
     }
   }